15cd9c7333238648a04d2c8bda8a37c566780f42
Saheer Khan <saheer.khan105@gmail.com>
2025-01-17 15:44:26 +0600

    sfds

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CrudApp extends StatelessWidget {
+  const CrudApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
